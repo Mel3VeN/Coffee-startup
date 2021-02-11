773c500a6ac003719670ab586889f83350a40c21
Melf Clausen <melfclausen@Melfs-MacBook-Air.local>
2021-02-11 14:59:35 +0100

    JS Functionality of Desktop nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,41 @@
-import myFunction, { multiply } from "./myModule";
+// change mavbar color after hero
 
-myFunction();
+// get  device screenheight
+// calculate 65% of screen height
+// after user scrolled past 65%, add eventlistener
+// change color
 
-const result = multiply(2, 5);
+// get height of hero
 
-console.log(result);
+const hero = document.getElementById("header");
+const navBar = document.getElementById("navbar");
+const target = hero;
+const logoWhite = document.getElementById("logoWhite");
+const logoBlack = document.getElementById("logoBlack");
+const link = document.getElementsByClassName("nav--link");
+const options = {
+  // root: hero,
+  rootMargin: "0px",
+  threshold: 0.1,
+};
+// CHECKS VIEWPORT !! NOT NAV
+const observer = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      navBar.classList.add("whiteBar");
+      for (let i = 0; i < link.length; i++) {
+        link[i].classList.add("blackLinks");
+      }
+      logoWhite.classList.add("logoDisplay");
+      logoBlack.classList.remove("logoDisplay");
+    } else {
+      navBar.classList.remove("whiteBar");
+      for (let i = 0; i < link.length; i++) {
+        link[i].classList.remove("blackLinks");
+      }
+      logoWhite.classList.remove("logoDisplay");
+      logoBlack.classList.add("logoDisplay");
+    }
+  });
+}, options);
+observer.observe(target);
